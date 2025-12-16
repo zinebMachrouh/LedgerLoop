05d7a817d338b4d8b9183de0e493726acdac8041
Zineb.machrouh <zinebmac49@gmail.com>
2025-12-16 10:16:25 +0100

    initiate projects

---
+++ banking-backend/src/test/java/com/skypay/banking/BankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypay.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
