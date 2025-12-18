bd1996186355cf3138ebb76667494edeb708afb1
Zineb.machrouh <zinebmac49@gmail.com>
2025-12-19 00:08:42 +0100

    refactor extract interface for repository

--- banking-backend/src/main/java/com/skypay/banking/repository/TransactionRepository.java
+++ banking-backend/src/main/java/com/skypay/banking/repository/TransactionRepository.java
@@ -1,32 +1,12 @@
 package com.skypay.banking.repository;
 
 import com.skypay.banking.model.Transaction;
-import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-@Repository
-public class TransactionRepository {
-
-    private final List<Transaction> transactions = new ArrayList<>();
-
-    public void save(Transaction transaction) {
-        transactions.add(transaction);
-    }
-
-    public List<Transaction> findAll() {
-        return new ArrayList<>(transactions);
-    }
-
-    public List<Transaction> findAllOrderByDateDesc() {
-        List<Transaction> sortedTransactions = new ArrayList<>(transactions);
-        Collections.reverse(sortedTransactions);
-        return sortedTransactions;
-    }
-
-    public void clear() {
-        transactions.clear();
-    }
+public interface TransactionRepository {
+    void save(Transaction transaction);
+    List<Transaction> findAll();
+    List<Transaction> findAllOrderByDateDesc();
+    void clear();
 }
